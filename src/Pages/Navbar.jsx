91cda48fdb80962233bea8ee683260104bfbc12a
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-11-05 23:53:12 +0600

    navbar create

---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,47 @@
+import { RiMenuAddFill } from "react-icons/ri";
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+
+
+const Navbar = () => {
+    const Links = [
+        { name: 'Home', link: '/' },
+        { name: 'About', link: '/about' },
+        { name: 'Service', link: '/service' },
+        { name: 'Contact', link: '/contact' }
+    ];
+    
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div className="w-full fixed top-0 left-0">
+            <div className="md:flex items-center justify-between bg-transparent py-4 md:px-10 px-7">
+                <div className="font-bold text-2xl cursor-pointer flex items-center">
+                    <p className="tracking-28p">BISTRO BOSS <br/><span className="tracking-38p">Restaurant</span></p>
+                </div>
+                {/* React icon nav */}
+                <div 
+                    onClick={() => setOpen(!open)} 
+                    className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+                >
+                    {open ? <IoClose /> : <RiMenuAddFill />}
+                </div>
+                <ul 
+                    className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20' : 'top-[-490px]'}`}
+                >
+                    {Links.map((link, index) => (
+                        <li key={index} className="md:ml-8 text-xl md:my-0 my-7">
+                            <a href={link.link} className="duration-500 text-white ">{link.name}</a>
+                        </li>
+                    ))}
+                   
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+
+
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      letterSpacing: {
+        '38p': '0.38em', // Use the equivalent em value for 38%
+        '28p'  : '0.23em'
+      },
+    },
   },
   plugins: [daisyui],
 }
